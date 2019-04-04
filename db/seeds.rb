a1f31d82c9ad01a636565dc287ee49a1ecb189d3
quang <quangdanang1@gmail.com>
2019-04-04 11:02:34 +0700

    tung

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
-User.create(email: 'admin@email.com', password: '123123', confirmed_at: DateTime.now, role: '2')
+# User.create(email: 'admin@email.com', password: '123123', confirmed_at: DateTime.now, role: '2')
+user = User.new(email: 'admin@email.com', password: '123123', confirmed_at: DateTime.now, role: '2')
+profile = user.build_profile(first_name: 'Admin', last_name: 'Admin')
+user.save!
