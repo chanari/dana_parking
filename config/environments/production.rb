ea08364b9afb00ff5d750f9d85e6e4f0a6d13985
chanari <quangdanang1@gmail.com>
2019-04-13 10:49:53 +0700

    quang update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.default_url_options = { host: 'https://blooming-peak-34925.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
