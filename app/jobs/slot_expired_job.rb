2770089731aa73bd2d2c7020902ef412ab2d1e32
chanari <quangdanang1@gmail.com>
2019-04-20 09:51:06 +0700

    quang update

--- app/jobs/slot_expired_job.rb
+++ app/jobs/slot_expired_job.rb
@@ -5,7 +5,6 @@ class SlotExpiredJob < ApplicationJob
     @slot = ParkingSlot.find_by id: slot_id
     if @slot.present? && ['1','2'].include?(@slot.status)
       @slot.reserve_expired
-      # ActionCable.server.broadcast 'booking_channel', @slot.as_json(only: [:id, :status])
     end
   end
 end
--- app/models/parking_slot.rb
+++ app/models/parking_slot.rb
@@ -14,7 +14,7 @@ class ParkingSlot < ApplicationRecord
   end
 
   def reserve(number_plate, client)
-    return false if User.where(id: client, role: '0').empty?
+    return false if User.where(id: client, role: '0').empty? && where(client: client).empty?
     SlotExpiredJob.set(wait: 30.seconds).perform_later(self.id) if self.update(status: '1', date_in: DateTime.now, number_plate: number_plate, client: client)
   end
 
