8e45f98de296ac191db90d1b6bbe6ada611a5fe2
chanari <quangdanang1@gmail.com>
2019-05-13 09:33:39 +0700

    quang update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,8 +32,8 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'https://blooming-peak-34925.herokuapp.com'
-  config.action_mailer.asset_host = 'https://blooming-peak-34925.herokuapp.com'
+  config.action_controller.asset_host = 'https://danaparking.herokuapp.com'
+  config.action_mailer.asset_host = 'https://danaparking.herokuapp.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
@@ -90,7 +90,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.default_url_options = { host: 'https://blooming-peak-34925.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://danaparking.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
