e9e63b6fc643cf8b76fcb009a6ef7198982f393c
chanari <quangdanang1@gmail.com>
2019-05-13 13:39:38 +0700

    quang update

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,5 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
 
-  validates :number_plate, uniqueness: { case_sensitive: false }
+  validates :number_plate, uniqueness: { case_sensitive: false, scope: :user_id }
 end
