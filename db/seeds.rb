4b0d2ee66d69ffd207029a3431e4d5c39f3c2771
chanari <quangdanang1@gmail.com>
2019-05-29 17:12:17 +0700

    quang update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-# User.create(email: 'admin@email.com', password: '123123', confirmed_at: DateTime.now, role: '2')
 user = User.new(email: 'admin@email.com', password: '123123', confirmed_at: DateTime.now, role: '2')
 profile = user.build_profile(first_name: 'Admin', last_name: 'Admin')
-user.save!
+user.save
